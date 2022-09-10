e3441b1a9017cc7099c86a018c0354de49a7dc37
RyanZapp <r_zapp@u.pacific.edu>
2022-09-09 21:13:22 -0700

    Updated Simulation file with initial conditions and boundary conditions set

--- src/Stoked2Dxy (1).m
+++ src/Stoked2Dxy (1).m
@@ -24,43 +24,27 @@ t = linspace(0,t_final,N);
 u = zeros(J,J,N);
 v = zeros(J,J,N);
 
-%u0 = zeros(J,J);
+[u0,v0] = initCond(x,y,h,J,L,ht);
 
-u(2:(J-1),1,:) = 0.5*ones((J-2),1,N) ; %if error, start trouble shooting here. need to be able to enforce condition for all N
-%u(2:(J-1),J,:) = ones((J-2),1,N) ; %rationalized using volumetric flow for uniform cross sectional area Qin = Qout
-%direchlet/periodic condition
-M = 0.5*ones(J,J);
-for i = 2:J-1
-    for j = 2:J-2
-    M(i,j) = k(j);
-    end
-    M(i,J-1) = M(i,J-2);
+u(:,:,1) = u0;
+v(:,:,1) = v0;
+
+for i = 2:N
+    u(:,1,i) = u0(:,1);
+    v(:,1,i) = v0(:,1);
 end
-M(1,:) = 0;
-M(J,:) = 0;
-M(:,J) = 0;
-u(:,:,1) = M;
-v(:,:,1) = IC;
-v(:,1,:) = % we want to set velocity I.C. as a parabola that doesnt have much magnitude
-% The b.c. should be the first time step of that parabola...not sure where
-% the file I made for it went tho
-% set neumann
-% The solutions exist and are smooth for I.C. = 0, I.C. = polynomial, I.C.
-% = sine wave that has reverse and forward flow, and much more...I.e. this
-% solution is very stable (letting u0 = 0 is akin to having a system at
-% rest and then introducing a constant pressure gradient.) That being said,
-% it implies that we can just use 0 I.C. if we need to in our 2D testerd
+% By this line, the I.C. should be det, along with the B.C. for the left,
+% top, and bottom sides
 
 for n = 1:N-1
     for i = 2:J-1
         for j = 2:J-2
            u(i,j,n+1) = deltaT*nu/(h^2)*(u(i+1,j,n) + u(i,j+1,n) - 4*u(i,j,n) + u(i-1,j,n) + u(i,j-1,n)) - deltaT*u(i,j,n)/h*(u(i+1,j,n) - u(i,j,n)) - deltaT*v(i,j,n)/h*(u(i,j+1,n) - u(i,j,n)) + u(i,j,n) ;
            v(i,j,n+1) = deltaT*nu/(h^2)*(v(i+1,j,n) + v(i,j+1,n) - 4*v(i,j,n) + v(i-1,j,n) + v(i,j-1,n)) - deltaT*u(i,j,n)/h*(v(i+1,j,n) - v(i,j,n)) - deltaT*v(i,j,n)/h*(v(i,j+1,n) - v(i,j,n)) + v(i,j,n) ;
-           %u(j,i,n+1) = deltaT*(deltaP/(rho*L) + nu/(h^2)*(u(j+1,i,n) + u(j,i+1,n) - 4*u(j,i,n) + u(j-1,i,n) + u(j,i-1,n)) - u(j,i,n)/h*(u(j+1,i,n) - u(j,i,n)) ) + u(j,i,n) ;
 
         end
-           u(i,J-1,n+1) = u(i,J-2,n+1);
-           v(i,J-1,n+1) = v(i,J-2,n+1);
+           u(i,J-1,n+1) = u(i,J-2,n+1); % Impose Neumann B.C. on rhs for x velocity
+           v(i,J-1,n+1) = v(i,J-2,n+1); % Impose Neumann B.C. on rhs for y velocity
     end
     
 end
