fb18324501792406f10df50c2078b98424229005
RyanZapp <r_zapp@u.pacific.edu>
2022-10-13 13:41:06 -0700

    Updated Laplacian Constructor

--- src/pressure_matrix.m
+++ src/pressure_matrix.m
@@ -1,51 +1,37 @@
 function L = pressure_matrix(nx,ny,dxi2,dyi2)
 
-% nx and ny are the sizes of the block segments
-% they are not the size of the resultant laplacian
-% the size of the laplacian is nx^2
-% so we just need to pass in nx and square root it
-% obviously, this means that nx and ny need to
-% be perfect squares
 D = -2*(dxi2 + dyi2);
 Dx = -(dxi2 + 2*dyi2);
 Dy = -(2*dxi2 + dyi2);
 Dxy = -(dxi2 + dyi2);
-T1 = toeplitz([D dxi2 zeros(1,sqrt(nx)-2)]);
-T2 = toeplitz([0 dyi2 zeros(1,sqrt(nx)-2)]);
-I = eye(sqrt(nx),sqrt(ny));
+T1 = toeplitz([D dxi2 zeros(1,nx-2)]);
+T2 = toeplitz([0 dyi2 zeros(1,nx-2)]);
+I = eye(nx,ny);
 L = kron(T1,I) + kron(I,T2);
+
 L(1,1) = Dxy;
-L(sqrt(nx),sqrt(ny)) = Dxy;
-    for j = 2:(sqrt(nx)-1)
-        for i = 2:(sqrt(ny)-1)
+L(nx,ny) = Dxy;
+    for j = 2:(nx-1)
+        for i = 2:(ny-1)
             if i==j
                 L(i,j) = Dx;
             end
         end
     end
-    L(end,end) = Dxy;
-    L(nx-sqrt(nx)+1,ny-sqrt(ny)+1) = Dxy;
-    for j = (nx-sqrt(nx)+2):(nx-1)
-        for i = (ny-sqrt(ny)+2):(ny-1)
+L(end,end) = Dxy;
+L(nx^2-nx+1,ny^2-ny+1) = Dxy;
+    for j = (nx^2-nx+2):(nx^2-1)
+        for i = (ny^2-ny+2):(ny^2-1)
             if i==j
                 L(i,j) = Dx;
             end
         end
     end
+ 
 
-    for j = (nx-sqrt(nx)+2):(nx-1)
-        for i = (ny-sqrt(ny)+2):(ny-1)
-            if i==j
-               L(i,j) = Dx;
-            end
-        end
-    end
-
-    c = 1;
-    while (c <= (sqrt(nx)-2))
-        L(c*sqrt(nx)+1,c*sqrt(ny)+1) = Dy;
-        L(c*sqrt(nx)+sqrt(nx),c*sqrt(ny)+sqrt(ny)) = Dy;
+c = 1;
+    while (c <= (nx-2))
+        L(c*nx+1,c*ny+1) = Dy;
+        L(c*nx+nx,c*ny+ny) = Dy;
         c = c+1;
     end
-end
-
