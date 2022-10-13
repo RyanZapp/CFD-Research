52a62fa0352fa79995d95eaa67cac5b5c2446628
RyanZapp <r_zapp@u.pacific.edu>
2022-10-13 14:21:03 -0700

    This file is essentially a copy pasted form of the file from the Chinese Github Page...It actually is a working simulation, so we can use it to learn where we are going wrong

---
+++ src/RipCodeFromHub.m
@@ -0,0 +1,88 @@
+Re = 300; % Reynolds number
+nt = 500; % max time steps (お試しで少しだけ)
+Lx = 1; Ly = 1; % domain size
+Nx = 80; Ny = 80; % Number of grids
+dt = 0.01; % time step;
+
+% Grid size (Equispaced)
+dx = Lx/Nx;
+dy = Ly/Ny;
+% Coordinate of each grid (cell center)
+xce = ((1:Nx)-0.5)*dx;
+yce = ((1:Ny)-0.5)*dy;
+% Coordinate of each grid (cell corner)
+xco = (0:Nx)*dx;
+yco = (0:Ny)*dy;
+
+u = zeros(Nx+1,Ny+2); % velocity in x direction (u)
+v = zeros(Nx+2,Ny+1); % velocity in y direction (v)
+p = zeros(Nx,Ny); % pressure (lagurange multiplier)
+
+uce = (u(1:end-1,2:end-1)+u(2:end,2:end-1))/2; % u at cell center
+vce = (v(2:end-1,1:end-1)+v(2:end-1,2:end))/2; % v at cell center
+
+[Xce,Yce] = meshgrid(xce,yce); % cell centerの座標グリッド
+[~,h_abs] = contourf(Xce',Yce',sqrt(uce.^2+vce.^2));
+
+xlim([0 Lx]); ylim([0 Ly]);
+
+for ii = 1:nt
+    bctop = 1; % Top lid u
+    u(:,1) = -u(:,2); v(:,1) = 0;             %bottom
+    u(:,end) = 2*bctop-u(:,end-1);  v(:,end) = 0;  %top
+    u(1,:) = 0;    v(1,:) = -v(2,:);             %left
+    u(end,:) = 0;  v(end,:) = -v(end-1,:);    %right
+    
+    Lux = (u(1:end-2,2:end-1)-2*u(2:end-1,2:end-1)+u(3:end,2:end-1))/dx^2; % nx-1 * ny
+    Luy = (u(2:end-1,1:end-2)-2*u(2:end-1,2:end-1)+u(2:end-1,3:end))/dy^2; % nx-1 * ny
+    Lvx = (v(1:end-2,2:end-1)-2*v(2:end-1,2:end-1)+v(3:end,2:end-1))/dx^2; % nx * ny-1
+    Lvy = (v(2:end-1,1:end-2)-2*v(2:end-1,2:end-1)+v(2:end-1,3:end))/dy^2; % nx * ny-1
+    
+    uce = (u(1:end-1,2:end-1)+u(2:end,2:end-1))/2;
+    uco = (u(:,1:end-1)+u(:,2:end))/2;
+    vco = (v(1:end-1,:)+v(2:end,:))/2;
+    vce = (v(2:end-1,1:end-1)+v(2:end-1,2:end))/2;
+    
+    % 2. multiply
+    uuce = uce.*uce;
+    uvco = uco.*vco;
+    vvce = vce.*vce;
+    
+    % 3-1. get derivative for u
+    Nu = (uuce(2:end,:) - uuce(1:end-1,:))/dx;
+    Nu = Nu + (uvco(2:end-1,2:end) - uvco(2:end-1,1:end-1))/dy;
+    % 3-2. get derivative for v
+    Nv = (vvce(:,2:end) - vvce(:,1:end-1))/dy;
+    Nv = Nv + (uvco(2:end,2:end-1) - uvco(1:end-1,2:end-1))/dx;
+    
+    % Get intermidiate velocity
+    u(2:end-1,2:end-1) = u(2:end-1,2:end-1) + dt*(-Nu + (Lux+Luy)/Re);
+    v(2:end-1,2:end-1) = v(2:end-1,2:end-1) + dt*(-Nv + (Lvx+Lvy)/Re);
+    
+    % velocity correction
+    % RHS of pressure Poisson eq.
+    b = ((u(2:end,2:end-1)-u(1:end-1,2:end-1))/dx ...
+        + (v(2:end-1,2:end)-v(2:end-1,1:end-1))/dy);
+    
+    % Solve for p (using cosine transform, faster)
+    p = solvePoissonEquation_2dDCT(b,Nx,Ny,dx,dy);
+    
+    % Direct method
+    % p = solvePoissonEquation_direct(b,Nx,Ny,dx,dy);
+    
+    % The new divergent free velocity field
+    u(2:end-1,2:end-1) = u(2:end-1,2:end-1) -  (p(2:end,:)-p(1:end-1,:))/dx;
+    v(2:end-1,2:end-1) = v(2:end-1,2:end-1) -  (p(:,2:end)-p(:,1:end-1))/dy;
+    
+    
+    
+     % get velocity at the cell center (for visualization)
+    uce = (u(1:end-1,2:end-1)+u(2:end,2:end-1))/2;
+    vce = (v(2:end-1,1:end-1)+v(2:end-1,2:end))/2;
+    
+    % update plot
+    h_abs.ZData = sqrt(uce.^2+vce.^2);
+    drawnow
+end
+    
+    
