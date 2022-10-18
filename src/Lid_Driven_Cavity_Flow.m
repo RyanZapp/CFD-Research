ac014e4999b51c91860486e655652f71675c8b53
RyanZapp <r_zapp@u.pacific.edu>
2022-10-17 21:46:46 -0700

    Most updated version of the lid driven cavity flow code that has the proper formulas and is very close to being correct, but not quite there

--- src/Lid_Driven_Cavity_Flow.m
+++ src/Lid_Driven_Cavity_Flow.m
@@ -3,8 +3,8 @@ close all
 clc
 rho = 1 ;
 nu = 0.008926 ;
-nx = 81;
-ny = 81;
+nx = 50;
+ny = 50;
 Lx = 1;
 Ly = 1;
 dx = (Lx-0)/nx;
@@ -16,11 +16,12 @@ dyi = 1/dy;
 dxi2 = dxi^2;
 dyi2 = dyi^2;
 
-nt = 10; % max number of timesteps
+nt = 50; % max number of timesteps
 
-dt = 0.00001*dx*dy/(nu*(dx + dy));% timestep size (determined by CFL condition)
-%dt = 0.125*dxi2/nu; % This is a backup timestep size in case the other one
+dt = dx*dy/(nu*(dx + dy));% timestep size (determined by CFL condition)
+dt = 0.125*dxi2/nu; % This is a backup timestep size in case the other one
 %fails
+dt = 0.01;
 
 % Specify location of cell centers (this is where we will be plotting)
 xce = ((1:nx)-0.5)*dx; % length(xce) = nx
@@ -49,6 +50,30 @@ vce = zeros(nx,ny);
 % efficiency
 
 L = pressure_matrix(nx,ny,dxi2,dyi2);
+% the things I did during this coding session were:
+% I took the inversion of L out of the loop, so that it only had to happen
+% once. I tried to use spectral decomposition to get L to invert without
+% being singular... I think I accomplished that task, but now everything
+% seems kinda worsse??? So chris and I will have to compare our laplacian
+% to the laplacian developed in the chinese code to really see what is
+% going on
+%LI = inv(L); 
+L(1,:) = 0;
+L(1,1) = 1; % I think the reason that some codes do this is to combat the problematic eigenvalue at the end
+[V,~] = eig(L);
+%LI = inv(L);
+lambda = eig(L);
+lambda(end) = [];
+lambda_reciprocal = 1./lambda;
+lambda_reciprocal(end+1) = 0; % use a pseudo inverse and set (1/lambda)=0 for lambda=0 
+D1_inv = diag(lambda_reciprocal);
+LI = transpose(V)*D1_inv*V;
+% basically, we have 1 eigenvector that is essentially 0, and that is our
+% problem....Im not sure why it is zero, or if having a zero eigenvector is
+% bad....all i know is that it is our current issue
+ % we have p V D V^T
+%p = V^T D^-1 V
+
 
 % to access the interior points of each matrix, type the following:
 % u(2:end-1,2:end-1);
@@ -134,13 +159,18 @@ for ii = 1:nt
             R1(i,j-1) = (us(i+1,j) - us(i,j))*0.5*dxi;
         end
     end
-    
+     % R1 messes up on the third iteration. It is already big on the third iteration, which means us is big on the third iteration, which means
+    % u must have been made big at the end of the 2nd iteration
     for j = 1:ny
         for i = 2:(nx+1)
             R2(i-1,j) = (vs(i,j+1) - vs(i,j))*0.5*dyi;
         end
     end
-    R = -rho/dt*(R1 + R2);
+    % again, on the 3rd iteration, R3 is huge right from the get go, which means vs was huge when calculating it, which means
+    % v must have been huge at the end of the 2nd iteration
+    R = -rho/dt*(R1 + R2); % R actually becomes huge at the 2nd passthrough
+    % R is so huge, so it does not matter if L is small, since R is going
+    % to accelerate the values anyways
     R = R(:);
     %L(1,:) = 0;
     %L(1,1) = 1; % for some reason, this stopped the matrix from being singluar....now, the only issue is that, for some reason
@@ -149,42 +179,53 @@ for ii = 1:nt
     % I do not know if the entries of R will match up with the
     % corresponding entries of the laplacian
     p = L\R;
-    p = reshape(p,nx,ny); 
-    LL = inv(L);
+    %p = LI*R; % Rather than using backslash, we can just invert the laplacian a single time before we ever enter the loop
+    % when i do L\R, I get a better looking result than when I spectrally
+    % decompose L and invert it that way
+    % In addition, when I use inv(L), it messes everything up, presumably
+    % because the "inv" command cannot handle near singular matrices...What
+    % ends up happening is I get an all constant matrix, so "inv"
+    % definitely is not the move...To double check, I should ask chris what
+    % happens when he uses the inv command
+    p = reshape(p,nx,ny); % p is huge at the 2nd go around, so thatt means 
+   % LL = inv(L); % L stays constant. R is the only thing that does not stay constant, so we can probably just "pre-invert"L
     
     % after I fix L, I just need to add the pressure derivatives back into
     % the solution
     
     for j = 2:(ny+1)
         for i = 2:nx
-            u(i,j) = us(i,j) - 1/rho*(p(i,j-1) - p(i-1,j-1))*0.5*dxi; % I think this one is correct, but it would not hurt to double check
-        end
-       
+            u(i,j) = us(i,j) - dt/rho*(p(i,j-1) - p(i-1,j-1))*0.5*dxi; % I think this one is correct, but it would not hurt to double check
+        end % so pressure somehow becomes huge, which is what is influencing u to get huge, which in turn, influences us to get huge
+       u(:,1) = 2*ubcb - u(:,2);
     end
-    u(:,1) = 2*ubcb - u(:,2);
+    
     u(:,end) = 2*ubct - u(:,end-1);
  
     u(1,:) = 0;
     u(end,:) = 0;
     for j = 2:ny
         for i = 2:(nx+1)
-            v(i,j) = vs(i,j) - 1/rho*(p(i-1,j) - p(i-1,j-1))*0.5*dyi; % I think this is right, but I should double check it
+            v(i,j) = vs(i,j) - dt/rho*(p(i-1,j) - p(i-1,j-1))*0.5*dyi; % I think this is right, but I should double check it
         end
+         v(1,:) = 2*vbcl - v(2,:);         % y-velocity left B.C.
+         v(end,:) = 2*vbcr - v(end-1,:);   % y-velocity right B.C.
     end
+    v(:,1) = 0;                     
+    v(:,end) = 0;  
     % something weird is happening in the middle row of the v matrix
     for j = 1:ny
         for i = 1:nx
             uce(i,j) = 0.5*(u(i,j+1) + u(i+1,j+1));
             vce(i,j) = 0.5*(v(i+1,j+1) + v(i+1,j));
         end
-        v(1,:) = 2*vbcl - v(2,:);         % y-velocity left B.C.
-        v(end,:) = 2*vbcr - v(end-1,:);   % y-velocity right B.C.
+       
     end
-    v(:,1) = 0;                     
-    v(:,end) = 0;                    
+                  
   
     h_abs.ZData = sqrt(uce.^2+vce.^2);
     drawnow
+    pause(0.025)
 % There are more efficient ways to do this code, but let us just make sure
 % it works for now
 
@@ -199,4 +240,73 @@ for ii = 1:nt
 
 % Bottom line is, the velocity values are exploding probably due to a
 % singularity sometwhere)
+
+% The changes I made were on lines 197 and line 208. I realized that I was
+% not multiplying the pressure term by dt. I was looking at videos and at
+% the papaer, and apparently, that is what you are supposed to do...not
+% 100% sure why because it did not 100% come up in the derivation.
+
+
+% The reason that things were (and possibly still are) exploding to
+% infinity is because when we assemble the R matrix, we divide by
+% dt...because dt was SUPER small, it was making R super huge. although
+% L^-1 was small, R was big enough to the point where it did not matter and
+% L^-1*R was really big... and because p=L^-1*R, obviously, p is super big
+% as a result. And then becasue we were adding the derivative of p to us,
+% even though us was correct (i.e. it was small), obviously, adding a huge
+% value in the form of p, to a small value in the form of us, still
+% generates a huge value.
+
+% That is when I looked at all of the formulas and things onliner and I
+% realized that when we add the pressure derivative back into us and vs, we
+% are supposed to multiply by dt to offset the absolute massive value of p.
+
+% doing this actually ended up working, however, it sort of just staved off the
+% inevitable, and things would run, but still end up crashing later
+
+% Then I tried redoing all the combinations of the laplacian to see if that
+% would help... WHat I noticed (and I will indeed double check this) is
+% that using my own inverse, or pseudo inverse, or anything for that
+% matter, would lead to a contour plot that wasnt even close to right. So I
+% started going back to using the backslash command. That seemed to produce
+% a musch more accurate plot...hoiwever, the matrix was still badly
+% conditioned. IN order to combat that, I treid to use the other piece of
+% information from the other paper where we set the top row of the
+% laplacian equal to all zeros and then set the first entry in the matrix
+% equal to 1 in order to stop the zero eigenvalue from appearing. THis got
+% rid og the warning messages that the matrix was singular, which was nice,
+% but the solution just appeared to oscillate through the first stages of
+% the process..
+
+% Now that I am looking back on it, I think what might be happening is the
+% fact that maybe the pressure matrix is getting bigger and bigger until we
+% reach the point where we just get NaNs, so no matter what, even if the
+% computations are correct, once we exceed a certain value size on the
+% computer, we will jsut get NaN computations or inf computations
+
+
+% WHat this means is that we can probably put a break point just after line
+% 171 to investigate how line 171 (which is the initialization of R)
+% changes as the simulation continues....this might be able to tell us if
+% we are running into an issue similar to what happened at the lab, where
+% the numbers were just too big for t
+
+
+% A final note on the matrix choice: I think that the backslash command or
+% the linsolve commands are our best bets becasue my inversion method is
+% trying to output complex values when the  grid gets
+% refined....additionally, the plots for my inversion method look a bit
+% bogus, but when I use the backslash command, they look like they are on
+% the right track, so we should stick with that for now
+
+
+% other than checking the increase in size of R, the only other thing that
+% needs to be checked is how our outputs for u,v,uce, and vce compare to
+% the outputs from the chinese github
+
+
+% The only difference between when I set the values in the first row of the
+% laplacian vs when I dont is that when I do set them, the matrix is not
+% badly conditioned.......however, the plots between the two look almost
+% identical
 end
