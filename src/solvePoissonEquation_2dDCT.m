b9761c99b55f56dbd2fa5dc6df2a39015d96a0ca
CaivsCrassvs <113131882+CaivsCrassvs@users.noreply.github.com>
2022-12-22 16:24:11 -0800

    Update solvePoissonEquation_2dDCT.m
    rip from Chinese paper

--- src/solvePoissonEquation_2dDCT.m
+++ src/solvePoissonEquation_2dDCT.m
@@ -15,7 +15,7 @@ fhat = dct(dct(b)')'; % Same as above (Needs Signal Processing Toolbox instead)
 phat = fhat./(MWX+MWY);
 
 % The solution is not unique (uhat(0,0) = inf);
-% Here we fix the mean ( with kx=0,ky=0) to be 0
+% Here we fix the mean (with kx=0,ky=0) to be 0
 phat(1,1) = 0;
 
 % Inverse 2D DCT
